12dfece42ce559d515360934280a88a92cd4938f
Raphael Berger <raphaelmb@gmail.com>
2023-03-26 14:28:56 -0300

    add users e2e test + gyms controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbytGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbytGymsQuerySchema.parse(request.params)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
